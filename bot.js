225285b5e3412f1e1f637f9a5de429b6e68d3770
Zach Strenfel <zwstrenfel@gmail.com>
2016-01-08 13:05:14 -0800

    progress, but probably giving up to move to selenium

--- bot.js
+++ bot.js
@@ -7,46 +7,93 @@
  * //\\//\\//\\ Initiating.. Initiating .. //\\//\\//
  **/
 var Bot = (function() {
-	//default website for bot
-	var url = "http://www.supremenewyork.com/shop/all";
+  //item information for the bot
+  var options = {
+    keyword: 'supreme-hanes-thermal-crew-1-pack',
+    size: 'medium',
+    color: null
+  };
 
-	var navigate = function() {
-		chrome.tabs.create({'url': url}, function(tab) {
-			initSearch(tab);
-		});
-	};
+  //array of content scripts to be injected
+  var scripts = [
+    'selectItem.js',
+    'addToCart.js'
+  ];
 
-	var initSearch = function(tab) {
-		chrome.tabs.executeScript(tab.id, {
-			file: "actions.js"
-		})
-	};
+  /**
+   * Navigates to the correct page, as denoted by url variable passed into the start function
+   * script injection.
+   */
+  var navigate = function(url) {
+    chrome.tabs.create({'url': url}, function(tab) {
+      injectScript(tab)
+    });
+  };
 
-	var addToCart = function(tab) {
-		chrome.tabs.executeScript(tab.id, {
-			file: "addToCart.js"
-		})
-	};
+  /**
+   * Injects correct content script into the current active page on the tab
+   * @param  {Tab} tab -- tab where script should be injected
+   */
+  var injectScript = function(tab, options) {
+    chrome.tabs.executeScript(tab.id, {
+      file: scripts[actionCounter]
+    });
+    sendMessage(options);
+    //update action counter to relay next file name;
+    actionCounter ++;
+  };
 
-	var start = function() {
-		navigate();
-	};
+  var sendMessage = function(message) {
+    console.log('sending message');
+    chrome.tabs.query({active:true, currentWindow:true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {options: message}, function(response) {
+        console.log(response.message);
+      })
+    })
+  };
 
-	return {
-		start:start,
-		addToCart: addToCart
-	};
+  /**
+   * Begins the processes of the bot when the browser extension is clicked.
+   * @param  {String} url -- Url of the target website.
+   */
+  var start = function(url) {
+    navigate(url);
+  };
+
+  return {
+    start:start,
+    injectScript: injectScript
+  };
 })();
 
+//counter that denotes what file to inject in the current page
+var actionCounter = 0;
+
+
+//Listener for the initial browser extension click
 chrome.browserAction.onClicked.addListener(function(activeTab) {
-	Bot.start();
+  Bot.start("http://www.supremenewyork.com/shop/all");
 })
 
+//Listener for tab change --> indicates that page navigation is complete and new script needs to be injected
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	Bot.addToCart(tab);
+  if (changeInfo.url !== undefined) {
+    console.log(changeInfo.url + " " + actionCounter);
+    Bot.injectScript(tab);
+  }
 })
 
-//listens for success action, and injects script into the next page
-// chrome.runtime.onMessage.addListener(function(request, sender) {
+//Listener for message from content scripts
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ? "from the content script: " + sender.tab.url : "from the extension");
+    if (request.message === "error") {
+      console.log("error");
+    } else {
+      console.log('message recived');
+    }
+  }
+)
 
-// });
+
+
--- selectItem.js
+++ selectItem.js
@@ -1,33 +1,36 @@
 /**
- * actions.js
+ * selectItem.js
  * //\\//\\// Beep Beep //\\//\\//
  * Meat and potatos of the bot,
  * this script oes things.
  * //\\//\\//\\ WorkingInitiating.. Working .. //\\//\\//
  **/
-var WorkerBot = (function() {
-  //keywords are currently autoset in the code; should be changed once UI is created.
-  var keyword = 'child-of-hell-keychain';
+var selectItem = (function() {
 
-  var activate = function() {
+  var activate = function(options) {
+    var keyword = options.keyword;
+
     var productLinks = document.getElementById('container').getElementsByTagName("a");
     for (var i in productLinks) {
       var href = productLinks[i].href;
       if (href.includes(keyword)) {
         productLinks[i].click();
-        nextStep();
         break;
       }
     }
   };
-
-  // var sendMessage = function() {
-  //   chrome.runtime.sendMessage
-  // }
 
   return {
     activate: activate
   }
 })();
 
-WorkerBot.activate();
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ? "from the content script: " + sender.tab.url : "from the extension");
+    sendResponse({message: "Options Recieved in selectItem.js"})
+    selectItem.activate(request.options);
+  }
+)
+
